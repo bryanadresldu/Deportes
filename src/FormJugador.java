f8842fa9c816b1743bde3e1d15cafaa8768d0d48
POO <bryan.angulo01@epn.edu.ec>
2025-07-01 09:01:52 -0500

    forms

--- src/FormJugador.java
+++ src/FormJugador.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class FormJugador {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton agregarButton;
+    private JButton mostrarButton;
+    private JButton limpiarButton;
+    private JTable table1;
 }
