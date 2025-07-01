7303b7009372a4e70439f0679779be5e5a8fcb44
POO <bryan.angulo01@epn.edu.ec>
2025-07-01 08:50:10 -0500

    forms

---
+++ src/FormAdm.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class FormAdm {
+    private JPanel panel1;
+}
---
+++ src/FormLogin.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class FormLogin {
+    private JPanel panelLogin;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JComboBox comboBox1;
+    private JButton accesoButton;
+}
